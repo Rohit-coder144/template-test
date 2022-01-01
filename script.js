4cc7620f27f78aa90d0d803ff7504ce7e3d5abfd
Rohit-coder144 <rohitpal8588975034@gmail.com>
2022-01-01 00:18:42 -0700

    Quote

--- script.js
+++ script.js
@@ -1,16 +1,50 @@
 const quoteText = document.getElementById("quote")
 const authorText = document.getElementById("author")
+const twitterBtn = document.getElementById("twitter")
+const newQuoteBtn = document.getElementById("new-quote")
+const loader = document.getElementById('loader')
+const quoteContainer = document.getElementById('quote-container')
+
+
+// Show loading
+function loading() {
+    loader.hidden = false
+    quoteContainer.hidden = true
+}
+
+// Hide loading
+function complete() {
+    quoteContainer.hidden = false
+    loader.hidden = true
+}
 
 // Show new Quote
 function newQuote() {
+    loading()
     // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
-    quoteText.innerHTML = quote.text
-    authorText.innerHTML = quote.author
+    // Check if author field is blank and replace it with 'Unknown
+    quoteText.innerText = quote.text
+
+    if(!quote.author) {
+        authorText.textContent = 'Unknown'
+    } else {
+        authorText.textContent = quote.author
+    }
+    // Check Quote length to determing styling
+    if (quote.text.length > 120) {
+        quoteText.classList.add("long-quote")
+    } else {
+        quoteText.classList.remove('long-quote')
+    }
+    // Set Quote. Hide Loader
+    complete()
+    
 }
 
 // Get Quotes From API
 async function getQuotes() {
+    loading()
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl)
@@ -21,5 +55,17 @@ async function getQuotes() {
     }
 }
 
+// Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.innerText} - ${author.innerText}`
+    window.open(twitterUrl, '_blank')
+}
+
+// Event Listener
+newQuoteBtn.addEventListener("click", newQuote)
+twitterBtn.addEventListener("click", tweetQuote)
+
+
 // On Load
 getQuotes() 
+
